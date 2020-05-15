6ef764939cd45b72d63b0f83404f665fb577383d
Ana Medrano <amedrano.fernandez@gmail.com>
2020-05-15 12:30:31 -0700

    added Card component and updated saved and search pages

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,16 +2,24 @@ import React from 'react';
 import NavBar from "../components/NavBar/index";
 import Jumbotron from "../components/Jumbotron/index";
 import Container from "../components/Container/index"
+import Card from "../components/Card/index"
+import SearchBar from "../components/SearchBar/index"
 
 const Search = () => {
 
-  return (
-    <div className="App">
-        {/* <Container> */}
-          <Jumbotron />
-        {/* </Container> */}
-    </div>
-  );
+    return (
+        <div className="Search">
+            <Container>
+                <Jumbotron />
+            </Container>
+            <Container>
+                <SearchBar />
+            </Container>
+            <Container>
+            <Card />
+            </Container>
+        </div>
+    );
 }
 
 export default Search;
