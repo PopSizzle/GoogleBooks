7d51ed615d70ca158f26dce2b62f8fad8023566d
Ana Medrano <amedrano.fernandez@gmail.com>
2020-05-15 11:07:37 -0700

    added components and completed Jumbotron and Navbar

---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+function Navbar() {
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/"><img src="#"/></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/search">Search</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/saved">Saved</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+};
+
+
+export default Navbar;
