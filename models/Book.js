5c2171ab2e2afd964f739d52225263144857aa74
Sam Poppe <poppe.samuel@gmail.com>
2020-05-15 11:00:54 -0700

    finish models folder

---
+++ models/Book.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: {type: String, required: true},
+    authors: {type: Array, required: true},
+    description: {type: Text, required: true},
+    image: { type: String}, 
+    link: { type: String},
+    publishedDate: { type: String},
+    rating: { type: Number},
+    categories: {type: Array},
+    pageCount: { type: Number},
+})
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
